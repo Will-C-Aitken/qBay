ef03a4d395734587c000c48c0d6aa0235d0198d7
alexkf99 <44884828+alexkf99@users.noreply.github.com>
2021-09-26 19:11:02 -0400

    Added Product Entity
    Added Product class to data_entities.py

--- data_entities.py
+++ data_entities.py
@@ -13,3 +13,32 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+'''
+    class Product attributes:
+    productID - Product identifier
+    name - Name/title of the product
+    seller - Name of seller
+    price - Price of the item
+    description - Description of the product
+    size - Size of the product (e.g. S, L, OneSize, ...)
+    category - Category of item (e.g. clothes, electronics, ...)
+    img - Product image
+    quantity - Number of items being sold
+'''
+
+
+class Product(db.Model):
+    productID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    seller = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    size = db.Column(db.String(20), nullable=False)
+    category = db.Column(db.String(40), nulltable=False)
+    img = db.Column(db.String(20), nullable=False, default='default.jpg')
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Product %r>' % self.name
+
