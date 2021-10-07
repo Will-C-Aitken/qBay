11d4240aca3eb31f8e2001ca9b726e30ffad5ddd
Eissa Khan <eissa.khane@gmail.com>
2021-10-07 23:37:01 +0000

    added user updates

--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -25,3 +25,31 @@ def test_r2_1_login():
 
     user = login('test0@test.com', 1234567)
     assert user is None
+
+def test_r3_update():
+    '''
+    Testing:
+
+        R3-1: A user is only able to update his/her user name, shipping_address, and postal_code.
+        R3-2: Shipping_address should be non-empty, alphanumeric-only, and no special characters such as !.
+        R3-3: Postal code has to be a valid Canadian postal code.
+        R3-4: User name follows the requirements above.
+
+    (will be tested after the previous test, so we already have u0, 
+      u1 in database)
+    '''
+
+    result = login('test0@test.com', 123456, {'username': 'apache'})
+    assert result is not False
+    result = login('test0@test.com', 123456, {'username': ''})
+    assert result is False
+
+    result = login('test0@test.com', 123456, {'postal_code': 'N2P 4M1'})
+    assert result is not False
+    result = login('test0@test.com', 123456, {'postal_code': 'aaa 123'})
+    assert result is False
+
+    result = login('test0@test.com', 123456, {'shipping_address': '123 fake st'})
+    assert result is not False
+    result = login('test0@test.com', 123456, {'shipping_address': '123 fake st!'})
+    assert result is False
