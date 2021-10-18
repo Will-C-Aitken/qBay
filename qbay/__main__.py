506c8151a8883e7639a3b97308999776190ecea2
Will Aitken <will.aitken1@outlook.com>
2021-10-18 19:42:13 -0400

    Implemented Home Page
    * After logging in, user is taken to the home page.
    * The home page lists options that users can select to perform further
    action
    * The actions are to be implemented as new features that link the action
    to the backend

--- qbay/__main__.py
+++ qbay/__main__.py
@@ -1,21 +1,34 @@
 from qbay import *
-from qbay.cli import login_page, regsiter_page
+from qbay.cli import login_page, register_page, home_page
 
 
 def main():
     while True:
-        selection = input(
-            'Welcome. Please type 1 to login. Or type 2 register.')
+
+        # Upon starting app, user can login, register or quit
+        selection = input('Welcome. Please type 1 to login, 2 to register, ' 
+                          'or 3 to quit\n')
         selection = selection.strip()
+
+        # Login
         if selection == '1':
             user = login_page()
             if user:
-                print(f'welcome {user.username}')
-                break
+                print(f'Welcome {user.username}')
+                home_page()
             else:
-                print('login failed')
+                print('Login failed')
+
+        # Register
         elif selection == '2':
-            regsiter_page()
+            register_page()
+
+        # Quit
+        elif selection == '3':
+            break
+
+        else:
+            print('Invalid option')
 
 
 if __name__ == '__main__':
--- qbay/cli.py
+++ qbay/cli.py
@@ -1,19 +1,69 @@
 from qbay.models import login, register
 
 
+def home_page():
+    """
+    Home page user is greeted with after login
+    """
+
+    while True:
+
+        # Upon signing in, user can create or update a product, update their
+        # profile or exit to login page
+        print()
+        print('Please choose from the following options:')
+        print('(1) Create product')
+        print('(2) Update product')
+        print('(3) Update profile')
+        print('(4) Return to login page')
+        selection = input()
+        selection = selection.strip()
+
+        # Create product
+        if selection == '1':
+            create_product_page()
+
+        # Update product
+        elif selection == '2':
+            update_product_page()
+            
+        # Update profile
+        elif selection == '3':
+            update_profile_page()
+            
+        # Return to login
+        elif selection == '4':
+            break
+            
+        else:
+            print('Invalid option')
+
+
 def login_page():
-    email = input('Please input email')
-    password = input('Please input password:')
+    email = input('Please input email: ')
+    password = input('Please input password: ')
     return login(email, password)
 
 
-def regsiter_page():
-    email = input('Please input email:')
-    password = input('Please input password:')
-    password_twice = input('Please input the password again:')
+def register_page():
+    email = input('Please input email: ')
+    password = input('Please input password: ')
+    password_twice = input('Please input the password again: ')
     if password != password_twice:
-        print('password entered not the same')
+        print('Passwords entered are not the same')
     elif register('default name', email, password):
-        print('registration succceeded')
+        print('Registration succeeded')
     else:
-        print('regisration failed.')
+        print('Registration failed')
+
+
+def create_product_page():
+    return
+
+
+def update_product_page():
+    return
+
+
+def update_profile_page():
+    return
