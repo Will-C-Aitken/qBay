6ad8a0807c7354dd8e33e690c0bb962c4758cfa8
alexkf99 <44884828+alexkf99@users.noreply.github.com>
2021-11-07 21:32:37 -0500

    added \n to cli.py

--- qbay/cli.py
+++ qbay/cli.py
@@ -160,7 +160,7 @@ def create_product_page(user):
     Each product requires [1] a title (<80 alphanumeric characters), [2]
     a description (within 20-2000 characters), and [3] a price (within
     10-10,000 CAD). Each new product's title must be unique, and its
-    description must be longer than the given title.''')
+    description must be longer than the given title\n.''')
 
     title = input("Please enter product title: ").strip()
     description = input("Please enter product's description: ").strip()
